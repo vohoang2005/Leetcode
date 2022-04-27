f949efd42ff2b3be038cc6c73b3fffefa2dbdc8a
Chuong Vo <vohoang2005@gmail.com>
2022-04-27 15:44:51 -0700

    organize materials

---
+++ 2.Two-Sum/two-sum.js
@@ -0,0 +1,13 @@
+// Quickest Runtime
+var twoSum = function(nums, target) {
+    let map = new Map();
+    
+    for(let i = 0; i < nums.length; i++) {
+      let num1 = nums[i];
+      let num2 = target - num1;
+      if (map.has(num2)) {
+        return [i, map.get(num2)];
+      }
+      map.set(num1, i);
+    }
+};
