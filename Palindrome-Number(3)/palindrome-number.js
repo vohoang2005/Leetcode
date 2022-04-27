5c5730ae214ab4d66dc835d224ec88e85bb3668c
Chuong Vo <vohoang2005@gmail.com>
2022-04-27 14:45:41 -0700

    updated palindrome

---
+++ Palindrome-Number(3)/palindrome-number.js
@@ -0,0 +1,14 @@
+// Using Reverse to Solve Palindrome Problem.
+
+var isPalindrome = function(x) {
+    return x == x.toString().split('').reverse().join('');
+  };
+
+// 80% Runtime & 46% Memory
+var isPalindrome = function(x) {
+    let num = x.toString();
+    for(let i = 0; i < (num.length/2); i++) {
+        if(num[i]!=num[num.length-1-i]) return false;
+    }
+    return true
+};
