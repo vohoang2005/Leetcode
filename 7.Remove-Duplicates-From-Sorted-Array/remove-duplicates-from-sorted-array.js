7e5cfe3817efe40c4d0a56726007c37e025d42f7
vohoang2005 <vohoang2005@gmail.com>
2022-04-30 01:23:28 -0700

    solved

--- 7.Remove-Duplicates-From-Sorted-Array/remove-duplicates-from-sorted-array.js
+++ 7.Remove-Duplicates-From-Sorted-Array/remove-duplicates-from-sorted-array.js
@@ -2,8 +2,10 @@ var removeDuplicates = function(nums) {
     let i = 0;
     
     for(let j = 0; j < nums.length; j++ ) {
-        if (nums[j] != nums[i])
-            nums[++i] = nums[j];
+        if (nums[j] != nums[j - 1]) {
+            nums[i] = nums[j];
+            i++
+        }  
     }
-    return ++i;
+    return i;
 }
