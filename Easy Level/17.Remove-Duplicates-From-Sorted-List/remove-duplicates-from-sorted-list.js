5f3dc7abffad8c3c36475cc70fde13480933cbcb
vohoang2005 <vohoang2005@gmail.com>
2022-05-11 20:23:09 -0700

    solved

---
+++ Easy Level/17.Remove-Duplicates-From-Sorted-List/remove-duplicates-from-sorted-list.js
@@ -0,0 +1,13 @@
+// Good way to compare duplicates and to remove.
+
+var deleteDuplicates = function(head) {
+    let current = head;
+        while(current !== null && current.next !== null) {
+            if(current.val === current.next.val) {
+              current.next = current.next.next
+        } else {
+            current = current.next;
+        }
+    }
+    return head;
+};
