837f31e6a52af7e2bcbc523ce9ac30abab2959e6
vohoang2005 <vohoang2005@gmail.com>
2022-05-12 15:36:54 -0700

    updated solution

--- Easy Level/18.Merge-Sorted-Array/merge-sorted-array.js
+++ Easy Level/18.Merge-Sorted-Array/merge-sorted-array.js
@@ -21,3 +21,22 @@ var merge = function(nums1, m, nums2, n) {
     }
     nums1.sort((a,b)=>a-b);
 };
+
+// Longer version, a little on the easier to understand side.
+var merge = function(nums1, m, nums2, n) {
+    let first = m - 1;
+    let second = n - 1;
+    
+    for(let i = m + n - 1; i >= 0; i--) {
+      if(second<0) {
+        break;
+      }
+      if (nums1[first] > nums2[second]) {
+        nums1[i] = nums1[first];
+        first--;
+      } else {
+        nums1[i] = nums2[second];
+        second--;
+      }
+    }
+  };
