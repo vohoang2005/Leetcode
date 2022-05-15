1d54563820e6fabe8088adb0cfcab06b00e5bf36
vohoang2005 <vohoang2005@gmail.com>
2022-05-14 18:14:11 -0700

    solved

---
+++ Easy Level/20.Same-Tree/same-tree.js
@@ -0,0 +1,27 @@
+// Very easy to understand solution.
+var isSameTree = function(p, q) {
+	if (p == null && q == null) {
+		return true;
+	}
+    if ((p == null && q != null) || (q == null && p != null)) {
+		return false;
+	}
+    	if (p.val != q.val) {
+		return false;
+	}
+    let good_lefts = isSameTree(p.left, q.left);
+    let good_rights = isSameTree(p.right, q.right);
+    return good_lefts && good_rights;
+};
+
+// Simplify solution.
+var isSameTree = function(p, q) {
+    if(!p && !q) return true;
+    if(!p || !q || p.val !== q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+
+// Using JSON.stringify.
+var isSameTree = function(p, q) {
+    return JSON.stringify(p)===JSON.stringify(q)
+};
