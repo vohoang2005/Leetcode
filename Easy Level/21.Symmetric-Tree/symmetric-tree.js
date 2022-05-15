ecda44f6aef7878045ff1c6694a4a74e33a11a64
vohoang2005 <vohoang2005@gmail.com>
2022-05-15 16:27:15 -0700

    solved

---
+++ Easy Level/21.Symmetric-Tree/symmetric-tree.js
@@ -0,0 +1,13 @@
+// Very clear way of comparing the mirror.
+var isSymmetric = function(root) {
+  if(root === null) return true; 
+
+    return symmetryChecker(root.left, root.right);
+  };
+  function symmetryChecker(left, right) {
+      if(left == null && right == null) return true;
+      if(left == null || right == null) return false;
+      if(left.val !== right.val) return false;
+
+      return symmetryChecker(left.left, right.right) && symmetryChecker(left.right, right.left);
+}
