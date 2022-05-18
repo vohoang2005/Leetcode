f33a9c7a31fb2096d0e7eebc85ceb3e860a7297f
vohoang2005 <vohoang2005@gmail.com>
2022-05-18 10:29:50 -0700

    solved via BFS

---
+++ Easy Level/25.Minimum-Depth-Of-Binary-Tree/minimum-depth-of-binary-tree.js
@@ -0,0 +1,7 @@
+// BFS Solution
+var minDepth = function(root) {
+    if(!root) return 0;
+    if(root.left === null) return minDepth(root.right) + 1;
+    if(root.right === null) return minDepth(root.left) + 1;
+    return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+};
