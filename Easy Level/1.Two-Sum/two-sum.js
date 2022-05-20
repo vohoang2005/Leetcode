092fafa72c01fd5513c6c76f7b87a8f965413001
vohoang2005 <vohoang2005@gmail.com>
2022-05-19 21:35:05 -0700

    another solution

--- Easy Level/1.Two-Sum/two-sum.js
+++ Easy Level/1.Two-Sum/two-sum.js
@@ -11,3 +11,14 @@ var twoSum = function(nums, target) {
       map.set(num1, i);
     }
 };
+
+// Vanilla Solution.
+var twoSum = function(nums, target) {
+  for(let i=0;i<nums.length;i++) {
+      for(let j=i+1; j<nums.length;j++) {
+          if(nums[i]+nums[j] === target) {
+              return [i,j]
+          }
+      }
+  }
+};
